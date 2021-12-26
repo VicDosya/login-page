ee75214a58f5937a40aa96657913c99b31352766
dosya <vicdosya@gmail.com>
2021-12-26 19:34:00 +0200

    Updated the Code - the console logs an object. The if condition is simpler.

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-
-//Clicking the button will console log the data username, password and checkbox.
+//Clicking the button will console log the data username, password and checkbox as an object
 document.getElementById("buttonLogin").onclick = function() {
 
     let myUsername = document.getElementById("myUsername").value;
     let myPassword = document.getElementById("myPassword").value;
 
-    if (document.getElementById('myCheckBox').checked) {
-        console.log("Remember me: true");
-    }
-    else {
-        console.log("Remember me: false");
-    }
+// if checkbox is checked, print true, if unchecked - false.
 
-    console.log("Username: ", myUsername);
-    console.log("Password: ", myPassword);
+    let rEqual = "false";
+if (document.getElementById('myCheckBox').checked) {
+    rEqual = "true";
+}
+
+//print in console the data as an object.
+
+    console.log({
+        Username: myUsername,
+        Password: myPassword,
+        RememberMe: rEqual
+    });
+
+//If username or password are empty - error message as alert.
 
     if(myUsername.length === 0 || myPassword.length === 0) {
         alert("Please enter your username and password correctly.");
