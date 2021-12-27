95741fd67f27585d17c55263be6a23823c7764c9
dosya <vicdosya@gmail.com>
2021-12-27 22:27:51 +0200

    HTML code moved to body section instead of head. container is now in a class and not id. Correct spacing between all the code. checkbox is now declared as a boolean instead of strings. Variable rEqual is now rememberMe. console logs the object as loginData object. pointless br's are now removed in html and fixed with CSS.

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
 //Clicking the button will console log the data username, password and checkbox as an object
-document.getElementById("buttonLogin").onclick = function() {
+document.getElementById("buttonLogin").onclick = function () {
 
     let myUsername = document.getElementById("myUsername").value;
     let myPassword = document.getElementById("myPassword").value;
-
-// if checkbox is checked, print true, if unchecked - false.
-
-    let rEqual = "false";
-if (document.getElementById('myCheckBox').checked) {
-    rEqual = "true";
-}
-
-//print in console the data as an object.
-
-    console.log({
+    let rememberMe = document.getElementById('myCheckBox').checked;
+    let loginData = {
         Username: myUsername,
         Password: myPassword,
-        RememberMe: rEqual
-    });
-    
-//If username or password are empty - error message as alert.
+        RememberMe: rememberMe
+    }
 
-    if(myUsername.length === 0 || myPassword.length === 0) {
+    //print in console the data as an object.
+
+    console.log(loginData);
+
+    //If username or password are empty - error message as alert.
+
+    if (myUsername.length === 0 || myPassword.length === 0) {
         alert("Please enter your username and password correctly.");
     }
 }
